fffda8c80dc427f670161aa7320825609e34fc4e
oskofo2022 <101888736+oskofo2022@users.noreply.github.com>
2023-02-02 21:58:28 -0300

    Update docker_push.sh

--- docker_push.sh
+++ docker_push.sh
@@ -3,8 +3,8 @@
 ##############################
 ## Get Environment Variables #
 ##############################
-source ./automation/docker_getenv.sh
-source ./automation/read_config.sh
+source ./Automation/docker_getenv.sh
+source ./Automation/read_config.sh
 
 #####################
 ## Docker get login #

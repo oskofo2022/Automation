3fbcc4e5d1f38c5bdbb3222c3fcea286dd8a47c5
oskofo2022 <101888736+oskofo2022@users.noreply.github.com>
2023-02-02 21:57:20 -0300

    Update docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # get variables form gitlab-ci or locals
-source ./automation/read_config.sh
-source ./automation/docker_getenv.sh
+source ./Automation/read_config.sh
+source ./Automation/docker_getenv.sh
 
 docker build $1 $2 \
         --build-arg VCS_REF=`git rev-parse --short HEAD` \
